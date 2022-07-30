37640fd2a21eca1326efd08f18953c744e95ab8e
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-07-30 19:38:51 -0400

    added more bootstrap components. Still working on getting flex-wrap to work how I want it

--- index.js
+++ index.js
@@ -82,6 +82,17 @@ function addEmployee() {
 }
 
 function startHtml() {
+  const css = `.team-card{
+    width: 18rem;
+  }
+.name-card{
+  font-size: 2rem;
+  }
+.title{
+  font-size: 2rem;
+}
+  `;
+
   const html = `<!DOCTYPE html>
   <html lang="en">
   <head>
@@ -93,10 +104,20 @@ function startHtml() {
     <title>The Whole Team</title>
   </head>
   <body>
-  <div class="container-fluid d-flex justify-content-center py-5 bg-primary text-light" style="font-size: 2rem">My Team</div>
+  <div class="container-fluid d-flex justify-content-center py-5 bg-primary text-light title">My Team</div>
+  <div class="container-fluid d-flex justify-content-center">
+  <div class="row">
+  <div class="col">
   <div class="card-deck mt-5">
   `;
+  // This will write the beginning of my roster.html file
   fs.writeFile("./dist/roster.html", html, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  // this will write the style.css file to my dist folder.
+  fs.writeFile("./dist/style.css", css, function (err) {
     if (err) {
       console.log(err);
     }
@@ -130,7 +151,7 @@ function addHtml(teammate) {
       `;
     } else if (role === "Engineer") {
       const github = teammate.getGithub();
-      insert = `<div class = "card team-card">
+      insert = `<div class= "card team-card">
       <div class="card-header bg-primary text-light name-card">${name}</div>
       <div class="card-body">
       <ul class="list-group list-group-flush">
@@ -147,7 +168,7 @@ function addHtml(teammate) {
       `;
     } else {
       const school = teammate.getSchool();
-      insert = `<div class = "card team-card">
+      insert = `<div class= "card team-card">
       <div class="card-header bg-primary text-light name-card">${name}</div>
       <div class="card-body">
       <ul class="list-group list-group-flush">
@@ -176,26 +197,14 @@ function addHtml(teammate) {
 function finishHtml() {
   const html = `
     </div>
+    </div>
+    </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     </body>
   </html> 
   `;
-
-  const css = `.team-card{
-      width: 18rem;
-    }
-  .name-card{
-    font-size: 2rem;
-    }
-    `;
-
-  fs.writeFile("./dist/style.css", css, function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
 
   fs.appendFile("./dist/roster.html", html, function (err) {
     if (err) {
